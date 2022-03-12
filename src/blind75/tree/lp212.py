a10674cd39a01feaa2ca76aa310410cbbabc0692
Jaime Abbariao <jaimeabbariao@Jaimes-MacBook-Pro.local>
2022-03-12 03:22:41 -0500

    feat: adding comments

--- src/blind75/tree/lp212.py
+++ src/blind75/tree/lp212.py
@@ -1,8 +1,10 @@
-from data_structures.tree import TreeNode
-
 from typing import List
 
 
+"""
+This problem requires us to use a Trie
+as well as some typical DFS to go through all the positions
+"""
 class Solution:
     def findWords(self, board: List[List[str]], words: List[str]) -> List[str]:
         pass
